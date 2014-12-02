b5f9a1b51f28af4ca336d382913aac7bcbedddf9
yaniv@jatakasource.org <yaniv@jatakasource.org>
2014-12-02 22:29:28 +0200

    Remove unused XML related classes.

--- jataka-common-web/src/main/java/org/jatakasource/web/xml/rendered/XMLRenderer.java
+++
@@ -1,9 +0,0 @@
-package org.jatakasource.web.xml.rendered;
-
-public interface XMLRenderer<T> {
-	public void setDelegated(T delegated);
-	public T getDelegated();
-	
-	static final String XML_NAME = "NAME";
-	static final String XML_DESCRIPTION = "DESCRIPTION"; 
-}
